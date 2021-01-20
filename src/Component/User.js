4aa72be8967bcfba20c46b59b3d7e82ef97f8d31
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-01-20 23:27:16 +0100

    enye-profile

--- src/Component/User.js
+++ src/Component/User.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
-
+import Modal from './Modal'
 const User = (props) => {
   const { list } = props;
+  const [details, setDetails]=useState({
+    open:false,
+    body:""
+  })
   const [state, setState] = useState({
     currentPage: 1,
     profilePerPage: 20,
@@ -10,7 +14,19 @@ const User = (props) => {
   var PageList = list
     ? list.slice(state.start, state.start + state.profilePerPage)
     : null;
-
+    const handleOpen = (list) => {
+      setDetails({
+        open:true,
+        body:list
+      })
+    };
+    const handleClose = () => {
+      setDetails({
+        ...details,
+        open:false
+        
+      })
+    };
   const handleNextPage = () => {
       if(state.currentPage < Math.ceil(list.length / 20)){
       setState({
@@ -38,17 +54,19 @@ const User = (props) => {
             <th>Gender</th>
             <th>Payment Method</th>
             <th>Email</th>
+            <th>Details</th>
           </tr>
         </thead>
         <tbody>
           {PageList?PageList.map((list, i) => (
             <tr key={i}>
               <td>{state.start + i}</td>
-              <td>{list.FirstName.toUpperCase()}</td>
+              <td >{list.FirstName.toUpperCase()}</td>
               <td>{list.LastName}</td>
               <td>{list.Gender}</td>
               <td>{list.PaymentMethod}</td>
               <td>{list.Email}</td>
+              <td className="modal"onClick={()=>handleOpen(list)}>View Details</td>
             </tr>
           )):
           <div className="loading">
@@ -62,6 +80,7 @@ const User = (props) => {
       
         <button onClick={handleNextPage}>Next Page</button>
       </div>
+     <Modal open={details.open} profile={details.body} handleClose={()=>handleClose()}/>
     </div>
   );
 };
